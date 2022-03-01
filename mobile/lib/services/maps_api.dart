0f03c08358e6aa97eb34339903c1bd821d27ab54
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-01 23:55:00 +0800

    Mobile: added place autocomplete, details and direction Maps APIs

--- mobile/lib/services/maps_api.dart
+++ mobile/lib/services/maps_api.dart
@@ -1,3 +1,4 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ride/utils/constants.dart';
 import 'package:ride/utils/request_helper.dart';
 
@@ -8,6 +9,31 @@ class MapsAPI {
   ) async {
     String url = 'https://maps.googleapis.com/maps/api/geocode/json?'
         'latlng=$latitude,$longitude&key=$kMapKey';
+    return await RequestHelper.get(url);
+  }
+
+  static Future<dynamic> placeAutoComplete(
+      String placeName, String sessionToken) async {
+    String url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?'
+        'input=$placeName&key=$kMapKey&sessiontoken=$sessionToken'
+        '&components=country:my';
+    return await RequestHelper.get(url);
+  }
+
+  static Future<dynamic> placeDetails(
+      String placeId, String sessionToken) async {
+    String url = 'https://maps.googleapis.com/maps/api/place/details/json?'
+        'place_id=$placeId&key=$kMapKey&sessiontoken=$sessionToken';
+    print('place details: $url');
+    return await RequestHelper.get(url);
+  }
+
+  static Future<dynamic> directions(
+      LatLng startPosition, LatLng endPosition) async {
+    String url = 'https://maps.googleapis.com/maps/api/directions/json?'
+        'origin=${startPosition.latitude},${startPosition.longitude}'
+        '&destination=${endPosition.latitude},${endPosition.longitude}'
+        '&mode=driving&key=$kMapKey';
     return await RequestHelper.get(url);
   }
 }
