4cdc613239b8ab9fde83101a38837c7d295af52c
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-09 23:05:05 +0800

    Mobile: added rideRequest into FireHelper

--- mobile/lib/utils/fire_helper.dart
+++ mobile/lib/utils/fire_helper.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:ride/models/nearby_driver.dart';
+import 'package:ride/models/ride_request.dart';
 
 class FireHelper {
   static List<NearbyDriver> _nearbyDriverList = [];
 
   static List<NearbyDriver> get nearbyDriverList => _nearbyDriverList;
+
+  static final _rideRequestsRef =
+      FirebaseDatabase.instance.ref('ride_requests');
 
   static void addNearbyDriver(NearbyDriver driver) {
     _nearbyDriverList.add(driver);
@@ -23,8 +27,32 @@ class FireHelper {
     _nearbyDriverList[index].longitude = driver.longitude;
   }
 
-  static Query getRideRequest(String tixId) {
-    final ticketListRef = FirebaseDatabase.instance.ref('request-tickets');
-    return ticketListRef.orderByChild('tix_id').equalTo(tixId);
+  static Future<void> addRideRequest(
+      String tixId, Map<String, Object> rideMap) async {
+    await _rideRequestsRef.child(tixId).set(rideMap);
+  }
+
+  static Future<RideRequest> getRideRequest(String tixId) async {
+    return await _rideRequestsRef.child(tixId).once().then((result) {
+      final Object? value = result.snapshot.value;
+      return RideRequest.parseRaw(value!);
+    });
+  }
+
+  static Future<void> updateRideRequest(
+      String tixId, Map<String, Object?> value) async {
+    await _rideRequestsRef.child(tixId).update(value);
+  }
+
+  static Future<void> removeRideRequest(String tixId) async {
+    await _rideRequestsRef.child(tixId).remove();
+  }
+
+  static Stream<DatabaseEvent> getWaitingRideRequestStream() {
+    return _rideRequestsRef.orderByChild('status').equalTo('waiting').onValue;
+  }
+
+  static Stream<DatabaseEvent> getRideRequestStreamByTixId(String tixId) {
+    return _rideRequestsRef.child(tixId).onValue;
   }
 }
