c675661dee2cdcea33e52e8d2f67d46cfc1093b9
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-19 23:40:33 +0800

    Mobile: added PendingTransaction for blockend

---
+++ mobile/lib/widgets/pending_transaction.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:ride/utils/ride_colors.dart';
+
+class PendingTransaction extends StatelessWidget {
+  const PendingTransaction({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        SpinKitPouringHourGlass(color: RideColors.secondaryColor),
+        SizedBox(height: 15),
+        Text(
+          'Pending Blockend Transaction...',
+          style: TextStyle(
+            fontSize: 12.0,
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'Please don\'t leave the app...',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ],
+    );
+  }
+}
