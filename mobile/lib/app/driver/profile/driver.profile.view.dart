f474d21f655efe8da0306ea017437ca5cca42ab5
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-20 23:52:08 +0800

    Mobile: added reveal private key for driver

--- mobile/lib/app/driver/profile/driver.profile.view.dart
+++ mobile/lib/app/driver/profile/driver.profile.view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ride/app/auth/auth.vm.dart';
 import 'package:ride/app/driver/profile/driver.profile.vm.dart';
 import 'package:ride/app/driver/widgets/driver_info.dart';
+import 'package:ride/app/passenger/widgets/alert.dart';
 import 'package:ride/widgets/paper_form.dart';
 import 'package:ride/widgets/paper_validation_summary.dart';
 
@@ -44,7 +46,7 @@ class DriverProfileView extends HookConsumerWidget {
               onPressed: () => context.go('/driver/update'),
             ),
             TextButton(
-              child: const Text('reset'),
+              child: const Text('Log out'),
               onPressed: () async {
                 await ref.read(authProvider.notifier).deleteAccount();
                 context.go('/');
@@ -52,6 +54,31 @@ class DriverProfileView extends HookConsumerWidget {
             ),
           ],
           children: <Widget>[
+            TextButton(
+              child: const Text('Reveal Your Private Key'),
+              onPressed: () => Alert(
+                title: 'Private key',
+                text:
+                    'WARNING, In production environment, the private key should be protected with password.\r\n\r\n'
+                    '${ref.read(authProvider.notifier).getPrivateKey() ?? "-"}',
+                actions: [
+                  TextButton(
+                    child: const Text('close'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  TextButton(
+                    child: const Text('copy and close'),
+                    onPressed: () {
+                      final privateKey =
+                          ref.read(authProvider.notifier).getPrivateKey();
+                      Clipboard.setData(ClipboardData(text: privateKey));
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ).show(context),
+            ),
+            const SizedBox(height: 20),
             driverProfile.when(
               loading: () => const CircularProgressIndicator(),
               error: (message) => PaperValidationSummary([message!]),
