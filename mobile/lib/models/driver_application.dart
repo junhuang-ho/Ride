4c0a9788a47ccd829944a3b521e05560910c8e10
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-27 18:56:08 +0800

    Mobile: added more field to DriverApplication

--- mobile/lib/models/driver_application.dart
+++ mobile/lib/models/driver_application.dart
@@ -2,12 +2,22 @@ class DriverApplication {
   late String driverId;
   late String createdAt;
   late String fileName;
+  late String email;
+  late String firstName;
+  late String lastName;
+  late String location;
+  late String phoneNumber;
   late String status;
 
   DriverApplication({
     required this.driverId,
     required this.status,
     required this.fileName,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.location,
+    required this.phoneNumber,
     required this.createdAt,
   });
 
@@ -15,6 +25,12 @@ class DriverApplication {
     driverId = (driverApplicant as dynamic)['driver_id'];
     status = (driverApplicant as dynamic)['status'];
     fileName = (driverApplicant as dynamic)['file_name'];
+    email = (driverApplicant as dynamic)['email'] ?? 'raydalio@gmail.com';
+    firstName = (driverApplicant as dynamic)['first_name'] ?? 'Ray';
+    lastName = (driverApplicant as dynamic)['last_name'] ?? 'Dalio';
+    location = (driverApplicant as dynamic)['location'] ?? 'New York';
+    phoneNumber =
+        (driverApplicant as dynamic)['phone_number'] ?? '+12332321212';
     createdAt = (driverApplicant as dynamic)['created_at'];
   }
 }
