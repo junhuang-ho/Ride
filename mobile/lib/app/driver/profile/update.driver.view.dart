16fff3e06f6ba6663a76cfea94a535eb8f9d9ec3
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-27 18:50:37 +0800

    Mobile: added email and phone to DriverUpdateProfile

--- mobile/lib/app/driver/profile/update.driver.view.dart
+++ mobile/lib/app/driver/profile/update.driver.view.dart
@@ -1,45 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ride/app/driver/profile/update.driver.vm.dart';
+import 'package:ride/utils/ride_colors.dart';
 import 'package:ride/widgets/paper_form.dart';
 import 'package:ride/widgets/paper_input.dart';
 import 'package:ride/widgets/paper_validation_summary.dart';
 
 class UpdateDriverView extends HookConsumerWidget {
-  const UpdateDriverView({Key? key}) : super(key: key);
+  const UpdateDriverView({
+    Key? key,
+    required this.profileInfoType,
+  }) : super(key: key);
+
+  final ProfileInfoType profileInfoType;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final updateDriver = ref.watch(updateDriverProvider);
     final updateDriverController = useTextEditingController();
 
+    String getLabelText() {
+      switch (profileInfoType) {
+        case ProfileInfoType.email:
+          return 'Email';
+        case ProfileInfoType.phone:
+          return 'Phone Number';
+        case ProfileInfoType.maxTravelDistance:
+          return 'Maximum Travel Distance';
+      }
+    }
+
+    String getHintText() {
+      switch (profileInfoType) {
+        case ProfileInfoType.email:
+          return 'your latest email address';
+        case ProfileInfoType.phone:
+          return 'your latest contact number';
+        case ProfileInfoType.maxTravelDistance:
+          return 'distance in metres';
+      }
+    }
+
+    TextInputType getKeyboardType() {
+      switch (profileInfoType) {
+        case ProfileInfoType.email:
+          return TextInputType.text;
+        case ProfileInfoType.phone:
+          return TextInputType.number;
+        case ProfileInfoType.maxTravelDistance:
+          return TextInputType.number;
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Update Profile'),
+        title: Text('Update ${getLabelText()}'),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.check),
+              onPressed: updateDriver.maybeWhen(
+                init: () => () async {
+                  switch (profileInfoType) {
+                    case ProfileInfoType.email:
+                      await ref
+                          .read(updateDriverProvider.notifier)
+                          .updateDriverProfileEmail(
+                              updateDriverController.text);
+                      break;
+                    case ProfileInfoType.phone:
+                      await ref
+                          .read(updateDriverProvider.notifier)
+                          .updateDriverProfilePhoneNo(
+                              updateDriverController.text);
+                      break;
+                    case ProfileInfoType.maxTravelDistance:
+                      await ref
+                          .read(updateDriverProvider.notifier)
+                          .updateMaxMetresPerTrip(updateDriverController.text);
+                      break;
+                  }
+                },
+                orElse: () => null,
+              )),
+        ],
       ),
       body: Center(
         child: PaperForm(
           padding: 30,
-          actionButtons: <Widget>[
-            OutlinedButton(
-              child: const Text('Update'),
-              onPressed: () async {
-                await ref
-                    .read(updateDriverProvider.notifier)
-                    .updateMaxMetresPerTrip(updateDriverController.text);
-              },
-            ),
-          ],
           children: <Widget>[
             updateDriver.when(
               init: () => PaperInput(
-                labelText: "Maximum Travel Distance",
-                hintText: 'Please type in distance in metres',
+                labelText: getLabelText(),
+                hintText: 'Please type in ${getHintText()}',
                 maxLines: 1,
                 controller: updateDriverController,
+                keyboardType: getKeyboardType(),
               ),
-              loading: () => const CircularProgressIndicator(),
+              loading: () => Column(
+                children: const [
+                  SpinKitSquareCircle(color: RideColors.primaryColor),
+                  SizedBox(height: 15),
+                  Text(
+                    'Updating Driver Profile...',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
               error: (message) => PaperValidationSummary([message!]),
               success: (data) => Text('Sucessfully updated $data!!!'),
             ),
--- mobile/lib/app/driver/profile/update.driver.vm.dart
+++ mobile/lib/app/driver/profile/update.driver.vm.dart
@@ -1,8 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ride/app/auth/auth.vm.dart';
 import 'package:ride/services/ride/ride_driver_registry.dart';
+import 'package:ride/utils/fire_helper.dart';
 
 part 'update.driver.vm.freezed.dart';
+
+enum ProfileInfoType { email, phone, maxTravelDistance }
 
 @freezed
 class UpdateDriverState with _$UpdateDriverState {
@@ -14,9 +18,11 @@ class UpdateDriverState with _$UpdateDriverState {
 
 class UpdateDriverVM extends StateNotifier<UpdateDriverState> {
   UpdateDriverVM(Reader read)
-      : _rideDriverRegistryService = read(rideDriverRegistryProvider),
+      : _authVM = read(authProvider.notifier),
+        _rideDriverRegistryService = read(rideDriverRegistryProvider),
         super(const UpdateDriverState.init());
 
+  final AuthVM _authVM;
   final RideDriverRegistryService _rideDriverRegistryService;
 
   Future<void> updateMaxMetresPerTrip(String maxMetresPerTrip) async {
@@ -26,6 +32,29 @@ class UpdateDriverVM extends StateNotifier<UpdateDriverState> {
       final result = await _rideDriverRegistryService
           .updateMaxMetresPerTrip(parsedMaxMetresPerTrip);
       state = UpdateDriverState.success(result);
+    } catch (ex) {
+      state = UpdateDriverState.error(ex.toString());
+    }
+  }
+
+  Future<void> updateDriverProfileEmail(String email) async {
+    try {
+      state = const UpdateDriverState.loading();
+      final driverId = await _authVM.getPublicKey();
+      await FireHelper.updateDriverApplication(driverId!, {"email": email});
+      state = UpdateDriverState.success(email);
+    } catch (ex) {
+      state = UpdateDriverState.error(ex.toString());
+    }
+  }
+
+  Future<void> updateDriverProfilePhoneNo(String phoneNumber) async {
+    try {
+      state = const UpdateDriverState.loading();
+      final driverId = await _authVM.getPublicKey();
+      await FireHelper.updateDriverApplication(
+          driverId!, {"phone_number": phoneNumber});
+      state = UpdateDriverState.success(phoneNumber);
     } catch (ex) {
       state = UpdateDriverState.error(ex.toString());
     }
