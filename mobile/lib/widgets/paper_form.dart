f49b98dc6b09c6c994cb52783dd402870f738934
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-29 21:19:16 +0800

    Mobile: added formKey and crossAxisAlignment to PaperForm

--- mobile/lib/widgets/paper_form.dart
+++ mobile/lib/widgets/paper_form.dart
@@ -3,23 +3,29 @@ import 'package:flutter/material.dart';
 class PaperForm extends StatelessWidget {
   const PaperForm({
     Key? key,
+    this.formKey,
     required this.children,
     this.padding = 8,
     this.actionButtons,
+    this.crossAxisAlignment,
   }) : super(key: key);
 
+  final Key? formKey;
   final List<Widget> children;
   final List<Widget>? actionButtons;
   final double padding;
+  final CrossAxisAlignment? crossAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: formKey,
       child: Container(
         constraints: const BoxConstraints(maxWidth: 480),
         child: Padding(
           padding: EdgeInsets.all(padding),
           child: Column(
+            crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.center,
             children: [
               ...children,
               ..._buildButtons(),
