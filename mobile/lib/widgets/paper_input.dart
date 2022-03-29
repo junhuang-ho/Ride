ff6edc424b3991fea4bfafdb0ab521abe0b14d46
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-29 21:18:32 +0800

    Mobile: added keyboardType to PaperInput

--- mobile/lib/widgets/paper_input.dart
+++ mobile/lib/widgets/paper_input.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:ride/utils/ride_colors.dart';
 
 class PaperInput extends StatelessWidget {
-  const PaperInput({
-    Key? key,
-    required this.labelText,
-    this.hintText,
-    this.errorText,
-    this.onChanged,
-    this.controller,
-    this.maxLines,
-    this.obscureText = false,
-  }) : super(key: key);
+  const PaperInput(
+      {Key? key,
+      required this.labelText,
+      this.hintText,
+      this.errorText,
+      this.onChanged,
+      this.controller,
+      this.maxLines,
+      this.obscureText = false,
+      this.keyboardType = TextInputType.text})
+      : super(key: key);
 
   final ValueChanged<String>? onChanged;
   final String? errorText;
@@ -19,6 +21,7 @@ class PaperInput extends StatelessWidget {
   final bool obscureText;
   final int? maxLines;
   final TextEditingController? controller;
+  final TextInputType keyboardType;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +30,17 @@ class PaperInput extends StatelessWidget {
       controller: controller,
       onChanged: onChanged,
       maxLines: maxLines,
+      keyboardType: keyboardType,
       decoration: InputDecoration(
+        focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: RideColors.primaryColor)),
+        errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: RideColors.errorColor)),
+        enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: RideColors.lightGrayColor)),
+        border: const OutlineInputBorder(),
+        errorStyle: const TextStyle(color: Colors.transparent),
+        helperText: '',
         labelText: labelText,
         hintText: hintText,
         errorText: errorText,
