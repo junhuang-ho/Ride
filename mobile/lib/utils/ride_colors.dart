5375feb746a4eb170c0d47e245e356b4f6b92865
wushuartgaro <hard-boiledw@hotmail.com>
2022-03-29 21:16:23 +0800

    Mobile: added more colors to RideColors

--- mobile/lib/utils/ride_colors.dart
+++ mobile/lib/utils/ride_colors.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
 class RideColors {
+  static const Color grey = Color(0xff3c3c3c);
+  static const Color backgroundColor = Color(0xff11111a);
+  static const Color cardColor = Color(0xff22222b);
+  static const Color primaryColor = Color(0xff007b80);
+  static const Color errorColor = Color(0xffea6060);
+  static const Color helperColor = Color(0xff787878);
+
   static const Color colorTextLight = Color(0xFF918D8D);
+
+  static const Color colorAccentPurple = Color(0xFF4f5cd1);
 
   static const Color colorGreen = Color(0xFF40cf89);
   static const Color colorLightGrayFair = Color(0xFFe1e5e8);
+  static const Color lightGrayColor = Color(0xffe6e6e6);
 }
+
+TextStyle helperStyle = const TextStyle(
+  fontSize: 16,
+  color: RideColors.helperColor,
+  letterSpacing: 0.5,
+  fontWeight: FontWeight.w500,
+);
