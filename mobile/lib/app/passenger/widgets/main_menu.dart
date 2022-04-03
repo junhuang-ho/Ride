5739c81f3fd7b0f10c81b62cb56fa6ba24e615cf
wushuartgaro <hard-boiledw@hotmail.com>
2022-04-03 22:30:08 +0800

    Mobile: added holdings and mnemonic to passenger main menu

--- mobile/lib/app/passenger/widgets/main_menu.dart
+++ mobile/lib/app/passenger/widgets/main_menu.dart
@@ -4,16 +4,19 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:jdenticon_dart/jdenticon_dart.dart';
 import 'package:ride/app/auth/auth.vm.dart';
+import 'package:ride/utils/ride_colors.dart';
 import 'package:ride/widgets/empty_content.dart';
 
 class MainMenu extends HookConsumerWidget {
   const MainMenu({
     Key? key,
     this.onReset,
+    this.onRevealMnemonic,
     this.onRevealKey,
   }) : super(key: key);
 
   final GestureTapCallback? onReset;
+  final GestureTapCallback? onRevealMnemonic;
   final GestureTapCallback? onRevealKey;
 
   @override
@@ -28,33 +31,51 @@ class MainMenu extends HookConsumerWidget {
             const HeaderSection(),
             const SizedBox(height: 5),
             ListTile(
-              title: const Text('Wallet'),
-              subtitle: const Text('View Account Balance and Holdings'),
-              leading: const Icon(Icons.wallet_membership),
+              title: const Text('Wallet', style: TextStyle(fontSize: 16)),
+              leading: const Icon(Icons.monetization_on),
               onTap: () => context.go('/passenger/wallet'),
             ),
-            const SizedBox(height: 5),
-            const Divider(height: 1, color: Color(0xffe2e2e2), thickness: 1),
             ListTile(
-              title: const Text('Private key'),
-              subtitle: const Text('Reveal your private key'),
+              title:
+                  const Text('Ride Holdings', style: TextStyle(fontSize: 16)),
+              leading: const Icon(Icons.token),
+              onTap: () => context.go('/passenger/holdings'),
+            ),
+            Consumer(
+              builder: (context, ref, _) {
+                final isMnemonicExist = ref.watch(authProvider.notifier
+                    .select((authVM) => authVM.isMnemonicExist));
+                if (isMnemonicExist) {
+                  return ListTile(
+                    title: const Text('Reveal Seed Phrase',
+                        style: TextStyle(fontSize: 16)),
+                    leading: const Icon(Icons.security),
+                    onTap: onRevealMnemonic,
+                  );
+                } else {
+                  return const SizedBox();
+                }
+              },
+            ),
+            ListTile(
+              title: const Text('Reveal private key',
+                  style: TextStyle(fontSize: 16)),
               leading: const Icon(Icons.vpn_key),
               onTap: onRevealKey,
             ),
             ListTile(
-              title: const Text('Reset wallet'),
-              subtitle: const Text('Wipe all wallet data'),
-              leading: const Icon(
-                Icons.warning,
-                color: Colors.orange,
-              ),
+              title: const Text('Log out', style: TextStyle(fontSize: 16)),
+              leading: const Icon(Icons.logout),
               onTap: onReset,
             ),
+            const Divider(height: 1, color: RideColors.grey, thickness: 1),
             ListTile(
-              title: const Text('Drive with Ride'),
+              title:
+                  const Text('Drive with Ride', style: TextStyle(fontSize: 16)),
               leading: const Icon(Icons.drive_eta),
               onTap: () => context.go('/apply_driver'),
             ),
+            const SizedBox(height: 5),
           ],
         ),
       ),
@@ -70,7 +91,7 @@ class HeaderSection extends HookConsumerWidget {
     final auth = ref.watch(authProvider);
 
     return SizedBox(
-      height: 250,
+      height: 200,
       child: DrawerHeader(
         child: auth.when(
           loading: () => const Center(
@@ -85,26 +106,53 @@ class HeaderSection extends HookConsumerWidget {
             message: '',
           ),
           authenticated: (accountData) => Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15),
               CircleAvatar(
-                child: SvgPicture.string(
-                  Jdenticon.toSvg(accountData.publicKey),
+                radius: 25,
+                backgroundColor: RideColors.primaryColor,
+                child: CircleAvatar(
+                  child:
+                      SvgPicture.string(Jdenticon.toSvg(accountData.publicKey)),
+                  radius: 22,
                 ),
               ),
-              const SizedBox(width: 15),
-              Text(
-                accountData.publicKey,
-                style: const TextStyle(fontSize: 12, fontFamily: 'Brand-Bold'),
+              const SizedBox(height: 10),
+              const Text(
+                'Account 1',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              // if (accountData.accountType == AccountType.admin)
-              //   ListTile(
-              //     title: const Text('Admin Panel'),
-              //     subtitle: const Text('Ride Administration'),
-              //     leading: const Icon(Icons.admin_panel_settings),
-              //     onTap: () => context.go('/admin'),
-              //   ),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 55.0,
+                    child: Text(
+                      accountData.publicKey,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: RideColors.colorLightGrayFair,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 45.0,
+                    child: Text(
+                      accountData.publicKey
+                          .substring(accountData.publicKey.length - 4),
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: RideColors.colorLightGrayFair,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
