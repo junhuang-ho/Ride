e78bbec6a036af4412bc13dc648a47d9748413fa
wushuartgaro <hard-boiledw@hotmail.com>
2022-04-05 20:17:11 +0800

    Mobile: added RideDetailsContent into RideRequestCard

--- mobile/lib/app/driver/home/widgets/ride_request_card.dart
+++ mobile/lib/app/driver/home/widgets/ride_request_card.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:numberpicker/numberpicker.dart';
 import 'package:ride/app/driver/home/driver.ride.vm.dart';
-import 'package:ride/app/driver/widgets/taxi_button.dart';
+import 'package:ride/app/driver/widgets/ride_button.dart';
 import 'package:ride/models/ride_request.dart';
 import 'package:ride/utils/ride_colors.dart';
+import 'package:ride/widgets/ride_details_content.dart';
 
 class RideRequestCard extends HookConsumerWidget {
   const RideRequestCard({
@@ -20,45 +20,34 @@ class RideRequestCard extends HookConsumerWidget {
     final useBadge = useState<int>(0);
 
     return Card(
+      elevation: 5.0,
+      margin: const EdgeInsets.all(0),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+        borderRadius: BorderRadius.circular(15.0),
       ),
-      color: Theme.of(context).bottomAppBarColor,
+      color: RideColors.cardColor,
       child: Container(
         margin: const EdgeInsets.all(4),
         child: Column(
           children: [
-            ListTile(
-              title: Text(rideRequest.destinationAddress),
-              subtitle: Text(
-                'from: ${rideRequest.pickupAddress} ${rideRequest.tixId}',
-              ),
+            RideDetailsContent(
+              userId: rideRequest.passengerId,
+              distanceText: rideRequest.distanceText,
+              durationText: rideRequest.durationText,
+              fare: rideRequest.tripFare,
+              pickupAddress: rideRequest.pickupAddress,
+              destAddress: rideRequest.destinationAddress,
             ),
-            const SizedBox(height: 10),
-            SizedBox(
-              height: 30,
-              width: 30,
-              child: NumberPicker(
-                zeroPad: true,
-                minValue: 0,
-                maxValue: 6,
-                value: useBadge.value,
-                onChanged: (value) {
-                  useBadge.value = value;
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: RideButton(
+                title: 'ACCEPT',
+                color: RideColors.primaryColor,
+                onPressed: () async {
+                  await ref.read(driverRideProvider.notifier).acceptRideRequest(
+                      rideRequest, useBadge.value.toString());
                 },
-                textStyle: const TextStyle(fontSize: 18),
-                haptics: true,
               ),
-            ),
-            const SizedBox(height: 10),
-            TaxiButton(
-              title: 'ACCEPT',
-              color: RideColors.colorGreen,
-              onPressed: () async {
-                await ref
-                    .read(driverRideProvider.notifier)
-                    .acceptRideRequest(rideRequest, useBadge.value.toString());
-              },
             ),
           ],
         ),
