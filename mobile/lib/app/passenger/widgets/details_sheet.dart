1dfd1be312be111132b9dc4ca762c7c91dd6674f
wushuartgaro <hard-boiledw@hotmail.com>
2022-04-06 23:33:08 +0800

    Mobile: enhance passenger details sheet

--- mobile/lib/app/passenger/widgets/details_sheet.dart
+++ mobile/lib/app/passenger/widgets/details_sheet.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:numberpicker/numberpicker.dart';
-import 'package:ride/app/driver/widgets/taxi_button.dart';
+import 'package:ride/app/driver/widgets/ride_button.dart';
 import 'package:ride/app/passenger/home/passenger.ride.vm.dart';
+import 'package:ride/app/passenger/home/request.ticket.vm.dart';
+import 'package:ride/widgets/pending_transaction.dart';
+import 'package:ride/models/direction_details.dart';
+import 'package:ride/utils/eth_amount_formatter.dart';
 import 'package:ride/widgets/common_sheet.dart';
-import 'package:ride/app/ride/request.ticket.vm.dart';
 import 'package:ride/services/crypto.dart';
 import 'package:ride/services/ride/ride_passenger.dart';
 import 'package:ride/utils/ride_colors.dart';
+import 'package:ride/widgets/ride_details_card.dart';
 
 class DetailsSheet extends HookConsumerWidget {
   const DetailsSheet({
@@ -25,12 +29,13 @@ class DetailsSheet extends HookConsumerWidget {
     final badge = useState<int>(0);
     final strict = useState<bool>(false);
 
-    Future<void> _requestRide(int distance, int duration) async {
+    Future<void> _requestRide(
+        DirectionDetails directionDetails, BigInt tripFare) async {
       await ref.read(requestTicketProvider.notifier).requestTicket(
             badge.value.toString(),
             strict.value,
-            BigInt.from(distance),
-            BigInt.from(duration),
+            BigInt.from(directionDetails.distanceValue),
+            BigInt.from(directionDetails.durationValue),
           );
       final passengerAddress =
           await ref.read(cryptoProvider).getUserPublicAddress();
@@ -40,7 +45,12 @@ class DetailsSheet extends HookConsumerWidget {
           .requestTicketEvents()
           .listen((event) {
         if (event.sender == passengerAddress) {
-          ref.read(passengerRideProvider.notifier).requestRide(event.tixId);
+          ref.read(passengerRideProvider.notifier).requestRide(
+                event.tixId,
+                passengerAddress: event.sender,
+                directionDetails: directionDetails,
+                tripFare: tripFare,
+              );
         }
       });
     }
@@ -55,24 +65,27 @@ class DetailsSheet extends HookConsumerWidget {
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Row(
                 children: <Widget>[
-                  // Image.asset(
-                  //   'images/taxi.png',
-                  //   height: 70,
-                  //   width: 70,
-                  // ),
-                  // const SizedBox(width: 16),
+                  Image.asset(
+                    'images/taxi.png',
+                    height: 70,
+                    width: 70,
+                  ),
+                  const SizedBox(width: 16),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       const Text(
-                        'Distance',
-                        style:
-                            TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
+                        'Duration',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
+                        ),
                       ),
                       passengerRide.maybeWhen(
-                        orElse: () => const SizedBox(),
-                        direction: (directionDetails) => Text(
-                          directionDetails.distanceText,
+                        orElse: () => const SpinKitThreeBounce(
+                            size: 20.0, color: RideColors.primaryColor),
+                        direction: (directionDetails, tripFare) => Text(
+                          directionDetails.durationText,
                           style: const TextStyle(
                               fontSize: 16, color: RideColors.colorTextLight),
                         ),
@@ -84,81 +97,50 @@ class DetailsSheet extends HookConsumerWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                       const Text(
-                        'Duration',
-                        style:
-                            TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-                      ),
-                      passengerRide.maybeWhen(
-                        orElse: () => const SizedBox(),
-                        direction: (directionDetails) => Text(
-                          directionDetails.durationText,
-                          style: const TextStyle(
-                              fontSize: 16, color: RideColors.colorTextLight),
+                        'Fare',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w900,
                         ),
                       ),
+                      passengerRide.maybeWhen(
+                        orElse: () => const SpinKitThreeBounce(
+                            size: 20.0, color: RideColors.primaryColor),
+                        direction: (directionDetails, tripFare) {
+                          return Text(
+                            '\$${EthAmountFormatter(tripFare).formatFiat()}',
+                            style: const TextStyle(
+                                fontSize: 16, color: RideColors.colorTextLight),
+                          );
+                        },
+                      ),
                     ],
+                  ),
+                  passengerRide.maybeWhen(
+                    orElse: () => const SizedBox(),
+                    direction: (directionDetails, tripFare) {
+                      return IconButton(
+                        icon: const Icon(Icons.open_in_new),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (ctx) {
+                              return RideDetailsCard(
+                                distanceText: directionDetails.distanceText,
+                                durationText: directionDetails.durationText,
+                                fare: tripFare,
+                                pickupAddress: directionDetails.startAddress,
+                                destAddress: directionDetails.endAddress,
+                              );
+                            },
+                          );
+                        },
+                      );
+                    },
                   ),
                 ],
               ),
             ),
-            // const SizedBox(height: 22),
-            // Padding(
-            //   padding: const EdgeInsets.symmetric(horizontal: 16),
-            //   child: Row(
-            //     children: <Widget>[
-            //       const SizedBox(width: 16),
-            //       const Icon(
-            //         Icons.badge,
-            //         size: 18,
-            //         color: RideColors.colorTextLight,
-            //       ),
-            //       const SizedBox(width: 16),
-            //       const Text(
-            //         'Badge',
-            //         style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-            //       ),
-            //       const SizedBox(width: 5),
-            //       SizedBox(
-            //         height: 50,
-            //         width: 30,
-            //         child: NumberPicker(
-            //           zeroPad: true,
-            //           minValue: 0,
-            //           maxValue: 6,
-            //           value: badge.value,
-            //           onChanged: (value) {
-            //             badge.value = value;
-            //           },
-            //           textStyle: const TextStyle(fontSize: 18),
-            //           haptics: true,
-            //         ),
-            //       ),
-            //       const Icon(
-            //         Icons.keyboard_arrow_down,
-            //         color: RideColors.colorTextLight,
-            //         size: 16,
-            //       ),
-            //       const SizedBox(width: 16),
-            //       const Icon(
-            //         Icons.assignment_turned_in,
-            //         size: 18,
-            //         color: RideColors.colorTextLight,
-            //       ),
-            //       const SizedBox(width: 16),
-            //       const Text(
-            //         'Strict',
-            //         style: TextStyle(fontSize: 18, fontFamily: 'Brand-Bold'),
-            //       ),
-            //       const SizedBox(width: 5),
-            //       Switch(
-            //         value: strict.value,
-            //         onChanged: (value) {
-            //           strict.value = value;
-            //         },
-            //       )
-            //     ],
-            //   ),
-            // ),
             const SizedBox(height: 15),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -168,38 +150,37 @@ class DetailsSheet extends HookConsumerWidget {
 
                   return requestTicketVM.maybeWhen(
                     requesting: () => const CircularProgressIndicator(),
-                    requested: (_) => Column(
-                      children: const [
-                        Text('Pending Transaction...'),
-                        SizedBox(height: 15),
-                        Text('Please don\'t leave the app...'),
+                    requested: (_) => const PendingTransaction(),
+                    orElse: () => Row(
+                      children: [
+                        RideButton(
+                          title: 'Cancel',
+                          color: Colors.red,
+                          onPressed: () async {
+                            await ref
+                                .read(passengerRideProvider.notifier)
+                                .cancelRide();
+                          },
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: RideButton(
+                            title: 'Request Ride',
+                            color: passengerRide.maybeWhen(
+                                direction: (_, __) => RideColors.primaryColor,
+                                orElse: () => Colors.grey),
+                            onPressed: passengerRide.maybeWhen(
+                              direction: (directionDetails, tripFare) =>
+                                  () async {
+                                await _requestRide(directionDetails, tripFare);
+                              },
+                              orElse: () => null,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
-                    orElse: () => TaxiButton(
-                      title: 'REQUEST RIDE',
-                      color: passengerRide.maybeWhen(
-                          direction: (_) => RideColors.colorGreen,
-                          orElse: () => Colors.grey),
-                      onPressed: passengerRide.maybeWhen(
-                        direction: (directionDetails) => () async {
-                          await _requestRide(directionDetails.distanceValue,
-                              directionDetails.durationValue);
-                        },
-                        orElse: () => null,
-                      ),
-                    ),
                   );
-                },
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: TaxiButton(
-                title: 'CANCEL',
-                color: Colors.orange,
-                onPressed: () async {
-                  await ref.read(passengerRideProvider.notifier).cancelRide();
                 },
               ),
             ),
