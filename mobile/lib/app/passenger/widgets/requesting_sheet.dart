445153262d9a2bc379e8d9efd27c74ad9e4e7e2b
wushuartgaro <hard-boiledw@hotmail.com>
2022-04-07 15:48:38 +0800

    Mobile: enhance Passenger Requesting Sheet

--- mobile/lib/app/passenger/widgets/requesting_sheet.dart
+++ mobile/lib/app/passenger/widgets/requesting_sheet.dart
@@ -2,18 +2,22 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ride/app/driver/widgets/taxi_button.dart';
+import 'package:ride/app/driver/widgets/ride_button.dart';
+import 'package:ride/app/passenger/home/passenger.otw.info.vm.dart';
 import 'package:ride/app/passenger/home/passenger.ride.vm.dart';
+import 'package:ride/app/passenger/home/request.ticket.vm.dart';
 import 'package:ride/app/passenger/trip/passenger.trip.vm.dart';
+import 'package:ride/app/passenger/widgets/reputation_details.dart';
+import 'package:ride/widgets/pending_transaction.dart';
 import 'package:ride/app/qrcode.reader.view.dart';
 import 'package:ride/services/crypto.dart';
 import 'package:ride/services/ride/ride_passenger.dart';
 import 'package:ride/widgets/common_sheet.dart';
-import 'package:ride/app/ride/request.ticket.vm.dart';
 import 'package:ride/models/ride_request.dart';
 import 'package:ride/utils/constants.dart';
 import 'package:ride/utils/ride_colors.dart';
 import 'package:ride/widgets/empty_content.dart';
+import 'package:ride/widgets/ride_details_card.dart';
 
 class RequestingSheet extends HookConsumerWidget {
   const RequestingSheet({
@@ -50,28 +54,19 @@ class RequestingSheet extends HookConsumerWidget {
                 }
                 final rideRequest = RideRequest.parseRaw(rawRideRequest);
                 if (rideRequest.status == Strings.waiting) {
-                  return const Text('Waiting Driver Accept...');
+                  return WaitingAcceptContent(rideRequest: rideRequest);
+                } else if (rideRequest.status == Strings.accepted) {
+                  return OnTheWayContent(rideRequest: rideRequest);
                 } else if (rideRequest.status == Strings.arrived) {
                   ref
                       .read(passengerRideProvider.notifier)
                       .updateDriverArrived();
-                  return AcceptedContent(rideRequest: rideRequest);
+                  return ArrivedContent(rideRequest: rideRequest);
                 } else {
-                  return const Text('Driver On The Way...');
+                  return Text('Ride Request Status: ${rideRequest.status}');
                 }
               },
             ),
-            // acceptedTicket.when(
-            //   loading: () => const CircularProgressIndicator(),
-            //   error: (err, stack) => EmptyContent(
-            //     title: 'Something Goes Wrong',
-            //     message: err.toString(),
-            //   ),
-            //   data: (ticket) {
-            //     ref.read(passengerRideProvider.notifier).updateTicketAccepted();
-            //     return AcceptedContent(acceptedTicket: ticket);
-            //   },
-            // ),
             const SizedBox(height: 20),
             Consumer(
               builder: (context, ref, _) {
@@ -122,8 +117,125 @@ class RequestingSheet extends HookConsumerWidget {
   }
 }
 
-class AcceptedContent extends HookConsumerWidget {
-  const AcceptedContent({
+class WaitingAcceptContent extends StatelessWidget {
+  const WaitingAcceptContent({
+    Key? key,
+    required this.rideRequest,
+  }) : super(key: key);
+
+  final RideRequest rideRequest;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Waiting Driver Accept...',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.open_in_new),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (ctx) {
+                return RideDetailsCard(
+                  distanceText: rideRequest.distanceText,
+                  durationText: rideRequest.durationText,
+                  fare: rideRequest.tripFare,
+                  pickupAddress: rideRequest.pickupAddress,
+                  destAddress: rideRequest.destinationAddress,
+                );
+              },
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class OnTheWayContent extends HookConsumerWidget {
+  const OnTheWayContent({
+    Key? key,
+    required this.rideRequest,
+  }) : super(key: key);
+
+  final RideRequest rideRequest;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final passengerOnTheWayInfo = ref.watch(passengerOnTheWayInfoProvider(
+        PassengerOnTheWayInput(
+            driverAddress: rideRequest.driverId, tixId: rideRequest.tixId)));
+
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Driver\'s On The Way...',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.open_in_new),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) {
+                    return RideDetailsCard(
+                      userId: rideRequest.driverId,
+                      distanceText: rideRequest.distanceText,
+                      durationText: rideRequest.durationText,
+                      fare: rideRequest.tripFare,
+                      pickupAddress: rideRequest.pickupAddress,
+                      destAddress: rideRequest.destinationAddress,
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          color: RideColors.grey,
+          child: Container(
+            margin: const EdgeInsets.all(4),
+            height: 100,
+            width: 300,
+            child: passengerOnTheWayInfo.maybeWhen(
+              loading: () => const Center(child: CircularProgressIndicator()),
+              data: (driverReputation, ticket) {
+                return ReputationDetails(
+                  metresTravelled: driverReputation.metresTravelled,
+                  tripCompleted: driverReputation.countEnd,
+                  rating: driverReputation.totalRating /
+                      driverReputation.countRating,
+                );
+              },
+              error: (errorMsg) => Text(errorMsg!),
+              orElse: () => const SizedBox(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ArrivedContent extends HookConsumerWidget {
+  const ArrivedContent({
     Key? key,
     required this.rideRequest,
   }) : super(key: key);
@@ -151,6 +263,37 @@ class AcceptedContent extends HookConsumerWidget {
 
     return Column(
       children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Driver Arrived!',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.open_in_new),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) {
+                    return RideDetailsCard(
+                      userId: rideRequest.driverId,
+                      distanceText: rideRequest.distanceText,
+                      durationText: rideRequest.durationText,
+                      fare: rideRequest.tripFare,
+                      pickupAddress: rideRequest.pickupAddress,
+                      destAddress: rideRequest.destinationAddress,
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Consumer(
@@ -159,51 +302,28 @@ class AcceptedContent extends HookConsumerWidget {
 
               return passengerTripVM.maybeWhen(
                 startingTrip: () => const CircularProgressIndicator(),
-                onTheWay: (_) => Column(
-                  children: const [
-                    Text('Pending Transaction...'),
-                    SizedBox(height: 15),
-                    Text('Please don\'t leave the app...'),
-                  ],
-                ),
-                orElse: () => TaxiButton(
+                onTheWay: (_) => const PendingTransaction(),
+                orElse: () => RideButton(
                   title: 'Scan Driver QR Code',
                   color: Colors.green,
                   onPressed: () async {
-                    Navigator.of(context)
-                        .push(CupertinoPageRoute(builder: (context) {
-                      return QRCodeReaderView(
-                        onScanned: (scannedAddress) async {
-                          await _startTrip(scannedAddress.toString());
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(
+                        builder: (context) {
+                          return QRCodeReaderView(
+                            onScanned: (scannedAddress) async {
+                              await _startTrip(scannedAddress.toString());
+                            },
+                          );
                         },
-                      );
-                    }));
-                    // context.push(
-                    //   '/qrcode_reader',
-                    //   extra: (scannedAddress) async {
-                    //     await ref
-                    //         .read(passengerRideProvider.notifier)
-                    //         .startTrip(scannedAddress.toString(), rideRequest);
-                    //   },
-                    // );
+                      ),
+                    );
                   },
                 ),
               );
             },
           ),
         ),
-        // const SizedBox(height: 10),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 16),
-        //   child: TaxiButton(
-        //     title: 'Cancel',
-        //     color: Colors.orange,
-        //     onPressed: () async {
-        //       await ref.read(requestTicketProvider.notifier).cancelRequest();
-        //       await ref.read(passengerRideProvider.notifier).cancelRide();
-        //     },
-        //   ),
-        // ),
       ],
     );
   }
